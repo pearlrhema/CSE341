6bef78afd9c7b9c810659f6f767194887d4a8553
pearlrhema <pearlrhema8@gmail.com>
2025-05-14 20:57:51 +0100

    update connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,27 +1,66 @@
+// const dotenv = require('dotenv');
+// dotenv.config();
+// const MongoClient = require('mongodb').MongoClient;
+
+// let _db;
+
+// const initDb = (callback) => {
+//   if (_db) {
+//     console.log('Db is already initialized!');
+//     return callback(null, _db);
+//   }
+//   MongoClient.connect(process.env.MONGODB_URI)
+//     .then((client) => {
+//       _db = client;
+//       callback(null, _db);
+//     })
+//     .catch((err) => {
+//       callback(err);
+//     });
+// };
+
+// const getDb = () => {
+//   if (!_db) {
+//     throw Error('Db not initialized');
+//   }
+//   return _db;
+// };
+
+// module.exports = {
+//   initDb,
+//   getDb,
+// };
+
 const dotenv = require('dotenv');
 dotenv.config();
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
 
 let _db;
 
-const initDb = (callback) => {
+const client = new MongoClient(process.env.MONGODB_URI, {
+  tls: true, // ensure TLS is enabled
+});
+
+const initDb = async (callback) => {
   if (_db) {
     console.log('Db is already initialized!');
     return callback(null, _db);
   }
-  MongoClient.connect(process.env.MONGODB_URI)
-    .then((client) => {
-      _db = client;
-      callback(null, _db);
-    })
-    .catch((err) => {
-      callback(err);
-    });
+
+  try {
+    await client.connect();
+    _db = client;
+    console.log('MongoDB connection established');
+    callback(null, _db);
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+    callback(err);
+  }
 };
 
 const getDb = () => {
   if (!_db) {
-    throw Error('Db not initialized');
+    throw new Error('Db not initialized');
   }
   return _db;
 };
